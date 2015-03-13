a1630b8dbbf5a904de6f70a3c265dad74669b3b0
Jukka Nousiainen <jukka.nousiainen@ericsson.com>
2015-03-13 13:08:17 +0200

    .zsh symlink

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ ln -fs "${PWD}/vimrc" "${HOME}/.vimrc"
 ln -fs "${PWD}/emacs" "${HOME}/.emacs"
 ln -fs "${PWD}/tmux.conf" "${HOME}/.tmux.conf"
 ln -fs "${PWD}/zshrc" "${HOME}/.zshrc"
-ln -fs "${PWD}/zsh/oh-my-zsh" "${HOME}/.oh-my-zsh"
+ln -fs "${PWD}/zsh" "${HOME}/.zsh"
 
 if [ -e "${HOME}/.elisp" ]; then
     rm -rf "${HOME}/.elisp"
