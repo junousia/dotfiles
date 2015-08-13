567c4f6b87d1c058213f9da31861caa8734d753d
Jukka Nousiainen <jukka.nousiainen@gmail.com>
2015-08-13 13:34:30 +0300

    support for defining the install directory

--- install.sh
+++ install.sh
@@ -3,19 +3,27 @@
 git submodule init
 git submodule update
 
-ln -fs "${PWD}/vimrc" "${HOME}/.vimrc"
-ln -fs "${PWD}/emacs" "${HOME}/.emacs"
-ln -fs "${PWD}/tmux.conf" "${HOME}/.tmux.conf"
-ln -fs "${PWD}/zshrc" "${HOME}/.zshrc"
-ln -fs "${PWD}/zsh" "${HOME}/.zsh"
-ln -fs "${PWD}/gitconfig" "${HOME}/.gitconfig"
-
-if [ -e "${HOME}/.elisp" ]; then
-    rm -rf "${HOME}/.elisp"
+if [ $# -eq 0 ]; then
+  INSTALLDIR=$HOME
+else
+  INSTALLDIR=$1
 fi
 
-if [ ! -d "${HOME}/.vim/bundle" ]; then
+ln -fs "${PWD}/vimrc" "${INSTALLDIR}/.vimrc"
+ln -fs "${PWD}/emacs" "${INSTALLDIR}/.emacs"
+ln -fs "${PWD}/tmux.conf" "${INSTALLDIR}/.tmux.conf"
+ln -fs "${PWD}/zshrc" "${INSTALLDIR}/.zshrc"
+ln -fs "${PWD}/zsh" "${INSTALLDIR}/.zsh"
+ln -fs "${PWD}/gitconfig" "${INSTALLDIR}/.gitconfig"
+ln -fs "${PWD}/gnupg" "${INSTALLDIR}/.gnupg"
+ln -fs "${PWD}/password-store" "${INSTALLDIR}/.password-store"
+
+if [ -e "${INSTALLDIR}/.elisp" ]; then
+    rm -rf "${INSTALLDIR}/.elisp"
+fi
+
+if [ ! -d "${INSTALLDIR}/.vim/bundle" ]; then
     echo "exists"
-    mkdir -p $HOME/.vim/bundle
-    git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
+    mkdir -p $INSTALLDIR/.vim/bundle
+    git clone git://github.com/Shougo/neobundle.vim $INSTALLDIR/.vim/bundle/neobundle.vim
 fi
