dd61c4c2528fa119655e4ba2b43e758a757fa0f0
Jukka Nousiainen <jukka.nousiainen@gmail.com>
2015-08-14 14:34:04 +0300

    cleanup install.sh

--- install.sh
+++ install.sh
@@ -10,17 +10,10 @@ else
 fi
 
 ln -fs "${PWD}/vimrc" "${INSTALLDIR}/.vimrc"
-ln -fs "${PWD}/emacs" "${INSTALLDIR}/.emacs"
 ln -fs "${PWD}/tmux.conf" "${INSTALLDIR}/.tmux.conf"
 ln -fs "${PWD}/zshrc" "${INSTALLDIR}/.zshrc"
 ln -fs "${PWD}/zsh" "${INSTALLDIR}/.zsh"
 ln -fs "${PWD}/gitconfig" "${INSTALLDIR}/.gitconfig"
-ln -fs "${PWD}/gnupg" "${INSTALLDIR}/.gnupg"
-ln -fs "${PWD}/password-store" "${INSTALLDIR}/.password-store"
-
-if [ -e "${INSTALLDIR}/.elisp" ]; then
-    rm -rf "${INSTALLDIR}/.elisp"
-fi
 
 if [ ! -d "${INSTALLDIR}/.vim/bundle" ]; then
     echo "exists"
