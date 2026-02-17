79781990f1b5050df72d51dc96d4d87a8e83f659
Jukka Nousiainen <jukka.nousiainen@gmail.com>
2026-02-17 22:42:41 +0200

    oh-my-zsh

--- install.sh
+++ install.sh
@@ -9,6 +9,12 @@ if ! command -v chezmoi &>/dev/null; then
     sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
 fi
 
+# Install oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing oh-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
 # Initialize and apply dotfiles
 echo "Initializing and applying dotfiles..."
 ~/.local/bin/chezmoi init --apply https://github.com/junousia/dotfiles.git
